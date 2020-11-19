68540a178b2e5f8f557403aad1df48f784765801
saritasakure <74542151+saritasakure@users.noreply.github.com>
2020-11-19 17:00:54 +0530

    Create ForEachIAndterator03.js

---
+++ Module-03/ForEachIAndterator03.js
@@ -0,0 +1,11 @@
+import { LightningElement } from 'lwc';
+
+export default class ForEachAndIterator extends LightningElement {
+    contacts=[  
+        {id:'C1',Name:'John Abraham',Email:'ja@gmail.com'},
+        {id:'C2',Name:'Brad Pitt',Email:'bpitt@gmail.com'},
+        {id:'C3',Name:'Angelina Jolie',Email:'angelj@gmail.com'},
+        {id:'C4',Name:'Peter Parker',Email:'spiderman@marvelcomics.com'}
+
+    ];
+}
