64be2f5bbee35c7399a66a9ebba79107ad22f9fc
saritasakure <74542151+saritasakure@users.noreply.github.com>
2020-11-29 20:10:27 +0530

    Create quickCreateOpportunities.js

---
+++ Module06/QuickCreate/quickCreateOpportunities/quickCreateOpportunities.js
@@ -0,0 +1,8 @@
+import { LightningElement ,track} from 'lwc';
+
+export default class QuickCreateOpportunities extends LightningElement {
+    @track recordId;
+    successHandler(event){
+        this.recordId = event.detail.id;
+    }
+}
